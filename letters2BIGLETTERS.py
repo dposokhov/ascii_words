74925f7331b3770239924111232e3bae55911387
dposokhov <30771908+dposokhov@users.noreply.github.com>
2017-10-27 22:05:51 +0300

    Update and rename 2.py to letters2BIGLETTERS.py
    added changes related to config.py and file_output

---
+++ letters2BIGLETTERS.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+from config import letters
+
+file_output = open('output.txt', 'w')
+print('Use pls only characters of English alphabet. Other characters will not be displayed. Be patient')
+
+word = input('Please input word: ')
+print()
+word = word.lower()
+
+for string in range(len(letters['a'])):
+    for char in word:
+        if char in ('qwertyuio pasdfghjklzxcvbnm'):
+            for char_for_print in letters[char][string]:
+                if char_for_print == '':
+                    file_output.write(' ')
+                else:
+                    file_output.write(char.upper())
+            file_output.write(' ')
+        else:
+            continue
+    file_output.write('\n')
+
